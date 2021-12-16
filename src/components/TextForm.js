57a4ac5bbeaceea578d82ba0e9023d9d0c4a1154
amitmhra <amit.mehra244@gmail.com>
2021-12-16 15:49:13 +0530

    My Commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUppercase = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLowercase = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (e)=>{
+        setText(e.target.value);
+    }
+    const [text, setText] = useState('Enter text here');
+    return (
+        <>
+        <div className='container'>
+            <h2>{props.heading}</h2>
+            <div className="mb-3">
+                <textarea className="form-control" id="box" rows="6" value={text} onChange={handleOnChange}></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUppercase}>Convert to uppercase</button>
+            <button className="btn btn-primary" onClick={handleLowercase}>Convert to lowecase</button>
+        </div>
+        <div className="container my-4">
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length } words, and {text.length} character</p>
+            <p>{0.008 * text.split(" ").length } minutes to read</p>
+            <h3 className='my-3'>Preview</h3>
+            <p>{text}</p>
+            
+        </div>
+        </>
+    )
+}
