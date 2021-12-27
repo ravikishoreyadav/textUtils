30136ed862731861ea4d6a7d05875a532f450345
ravikishoreyadav <mydevitsolutions@gmail.com>
2021-12-27 18:05:42 +0530

    Committed by Ravi on App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" about="About" />
+      <Navbar title="TextUtils" about="About" mode={mode} toggleMode = {toggleMode}/>
       <div className="container my-3" >
         <TextForm heading="Enter the text to analyze below" />
       </div>
+      {/* <About/> */}
       
     </>
   );
